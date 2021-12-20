a347c862a848c74777aa8e99d67f141de76144e7
lauranavoni <lauranavonigomez@gmail.com>
2021-12-20 07:12:34 -0300

    add components cart shopping

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cards from "./Components/Cards";
+
+function App() {
+  return (
+    <div>
+      <h1>  Malana Deco </h1>
+      <br></br>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ src/Components/Cards.js
@@ -0,0 +1,54 @@
+
+import { useReducer } from "react";
+import { TYPES } from "../Action/shoppingCart";/*ready*/
+import {
+  shoppingInitialState,
+  shoppingReducer,
+} from "../Action/shoppingReducer"; /*ready*/
+import CartItem from "./CartItem"; /*ready*/ 
+import ProductItem from "./ProductItem"; /*ready*/
+
+const Cards = () => {
+  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+
+  const { products, cart } = state;
+
+  const addToCart = (id) => {
+    //console.log(id);
+    dispatch({ type: TYPES.ADD_TO_CART, payload: id });
+  };
+
+  const delFromCart = (id, all = false) => {
+    //console.log(id, all);
+    if (all) {
+      dispatch({ type: TYPES.REMOVE_ALL_FROM_CART, payload: id });
+    } else {
+      dispatch({ type: TYPES.REMOVE_ONE_FROM_CART, payload: id });
+    }
+  };
+
+  const clearCart = () => {
+    dispatch({ type: TYPES.CLEAR_CART });
+  };
+
+  return (
+    <div>
+      <h2>Ecommerce Malana Deco</h2>
+      <h3>Products</h3>
+      <article className="box grid-responsive">
+        {products.map((product) => (
+          <ProductItem key={product.id} data={product} addToCart={addToCart} />
+        ))}
+      </article>
+      <h3>Carrito</h3>
+      <article className="box">
+        <button onClick={clearCart}>Limpiar Carrito</button>
+        {cart.map((item, index) => (
+          <CartItem key={index} data={item} delFromCart={delFromCart} />
+        ))}
+      </article>
+    </div>
+  );
+};
+
+export default Cards
