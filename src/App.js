34a72cca1e258a0e4cb0d5320aef79714fb310a9
lauranavoni <lauranavonigomez@gmail.com>
2021-12-22 17:03:28 -0300

    fixed cart actions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import ProductAddToCart from "./Components/ProductAddtoCart";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+import Navbar from "./Components/NavBar";
+import Footer from "./Components/Footer";
+import Home from "./Screen/Home";
+import SingleProduct from "./Screen/Single_Product";
+import CartScreen from "./Screen/Cart_Screen";
 
 function App() {
   return (
-    <div>
-      <h1>  Aura deco</h1>
-      <br></br>
-  <ProductAddToCart></ProductAddToCart>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />}></Route>
+        <Route exact path='/cart'>
+          <CartScreen />
+        </Route>
+       
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
- 
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./Context/product_context";
+import { FilterProvider } from "./Context/filter_context";
+import { CartProvider } from "./Context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
