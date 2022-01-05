08ed3a6fb9100bb5b2ed44ef9c9020e509879e2f
lauranavoni <lauranavonigomez@gmail.com>
2022-01-05 09:21:53 -0300

    add navbar

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,51 +1,12 @@
-import React from 'react';
-import styles from './App.module.css';
-import Header from './Component/Header/Header';
-import CartProvider from './Component/store/CartProvider';
-import { useEffect, useState } from 'react';
-import { fetchProducts } from "../src/Api";
+//import Product from "./pages/Product";
+import Home from "./Pages/Home";
+//import ProductList from "./pages/ProductList";
+//import Register from "./pages/Register";
+//import Login from "./pages/Login";
+//import Cart from "./pages/Cart";
 
-export default function App() {
-const [products, setProducts] = useState (null);
-
-const getData = async () => {
-  setProducts ((await fetchProducts()) || []);
+const App = () => {
+  return <Home/>;
 };
 
-
-useEffect(() =>{
- getData();
-}, [])
-
-const cart = [{
-  id:'c1',
-  title:'Front End',
-  description:'Front End',
-  price:0,
-  image:'Front End'
- 
-}]
-
-const [cartVisible,setCartVisible] = useState(false);
-  const showCartHandler = ()=>{
-    setCartVisible(true);
-  } 
-  const hideCartHandler = ()=>{
-    setCartVisible(false);
-  }
-  return (
-    <><CartProvider className={styles.app}>
-      {cartVisible && <Cart onHideCart={hideCartHandler} cart={cart} />}
-      <Header onShowCart={showCartHandler} onHideCart={hideCartHandler} />
-  
-    </CartProvider><div>
-
-        <div className='App'>
-          <h1> Categories</h1>
-          {products.map((cat, idx) => {
-            return <div key={cat.id}>{cat.name}</div>;
-          })}
-        </div>
-      </div></>
-  );
-}
+export default App;
---
+++ reactjs/src/Component/Header/Footer/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => (
+    <footer>
+      <div>
+         <a href="https://codealo.dev">Codealo</a>
+      </div>
+      <div>
+        <a href="https://instagram.com/codealo">Siguenos en Instagram</a>
+      </div>
+    </footer>
+  );
+  
+  export default Footer;
