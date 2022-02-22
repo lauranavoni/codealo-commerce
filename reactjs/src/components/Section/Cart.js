dcdcffd69eb6eb0b34de1d3fca9963fa0b8a9bea
lauranavoni <lauranavonigomez@gmail.com>
2022-02-22 17:50:05 -0300

    add changes in production

--- reactjs/src/components/Section/Cart.js
+++ reactjs/src/components/Section/Cart.js
@@ -1,81 +1,55 @@
-import React from "react";
-import {useSelector} from 'react-redux'
-import { addCart, delCart } from '../redux/action/index'
-import { NavLink } from "react-router-dom";
-import {  useDispatch} from "react-redux";
+import React,{useContext} from 'react';
+import styles from '../Cart/Cart.modules.css';
+import Modal from '../UI/Modal'
+import CartItem from '../CartItem/CartItem';
+import CartContext from '../../store/CartContext';
 
 
-const Cart = () => {
-    const state = useSelector((state)=> state.handleCart)
-    const dispatch = useDispatch()
-
-    const handleAdd = (item) => {
-        dispatch(addCart(item))
+const Cart = props =>{
+    const cartCtxt = useContext(CartContext);
+    const hasItems = cartCtxt.items.length>0;
+    const cartItemAddHandler =(item) =>{
+        cartCtxt.addItem({...item,amount:1});
     }
-    const handleDel = (item) => {
-        dispatch(delCart(item))
+    const cartItemRemoveHandler = (id) =>{
+        cartCtxt.removeItem(id)
     }
-
-    const emptyCart = () => {
-        return(
-            <div className="px-4 my-5 bg-light rounded-3 py-5">
-                <div className="container py-4">
-                    <div className="row">
-                        <h3>Your Cart is Empty</h3>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-    const cartItems = (product) => {
-        return(
-            <>
-                <div className="px-4 my-5 bg-light rounded-3 py-5">
-                <div className="container py-4">
-                    <div className="row justify-content-center">
-                        <div className="col-md-4">
-                            <img alt= {product.img}src={`https://codealo-commerce-cms.onrender.com${product.image.url} `}  height="200px" width="180px" />
-                        </div>
-                        <div className="col-md-4">
-                            <h3>{product.title}</h3>
-                            <p className="lead fw-bold">
-                                {product.quantity} X ${product.price} = ${product.quantity * product.price}
-                            </p>
-                            <button className="btn btn-outline-dark me-4" onClick={()=>handleDel(product)}>
-                                ➖
-                            </button>
-                            <button className="btn btn-outline-dark" onClick={()=> handleAdd(product)}>
-                                ➕
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </>
-        )
-
-    }
-    const buttons = () => {
-        return(
-            <>
-                <div className="container">
-                    <div className="row">
-                        <NavLink to="/payment" className="btn btn-outline-dark mb-5 w-25 mx-auto">
-                         Payment
-                        </NavLink>
-                    </div>
-                </div>
-            </>
-        )
-    }
+    const cartItems = (
+    <ul className={styles['cart-items']}>
+        {
+            cartCtxt.items.map((item) => (
+                <CartItem 
+                    key={item.id} 
+                    name={item.name} 
+                    id={item.id} 
+                    amount={item.amount} 
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null,item.id)}
+                    onAdd={cartItemAddHandler.bind(null,item)}
+                />
+            )
+            )
+        }
+    </ul>
+    );
 
     return (
-        <div>
-            {state.length === 0 && emptyCart()}
-            {state.length !== 0 && state.map(cartItems)}
-            {state.length !== 0 && buttons()}
-        </div>
-    );
-}
+        <Modal onClick={props.onHideCart}>
+            {cartItems}
+            <div className={styles.total}>
+                <span>
+                    Total Amount
+                </span>
+                <span>
+                    ${cartCtxt.totalAmount.toFixed(2)}
+                </span>
+            </div >
+            <div className={styles.actions}>
+                <button className={styles["button--alt"]}onClick={props.onHideCart}>Close</button>
+                {hasItems && <button className={styles.button}>Buy</button>}
+            </div>
+        </Modal>
+    )
 
+}
 export default Cart;
